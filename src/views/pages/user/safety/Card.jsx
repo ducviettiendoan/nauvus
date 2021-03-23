e98395f31b2eb9f939adb5faf8f21cacfa415c11
Duc Doan <ducdoan@Apples-MacBook-Air.local>
2021-03-23 11:27:00 +0700

    update safety page

--- src/views/pages/user/safety/Card.jsx
+++ src/views/pages/user/safety/Card.jsx
@@ -7,11 +7,36 @@ import Button from "@material-ui/core/Button"
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Link from '@material-ui/core/Link';
 import StarIcon from '@material-ui/icons/Star';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import Select from "@material-ui/core/Select";
+import {makeStyles} from "@material-ui/core/styles";
+import FormControl from "@material-ui/core/FormControl";
 
+const styles = {
+    selectForm: {
+        width: "108px",
+        height: "44px",
+        background: "#FFFFFF",
+        boxSizing: "border-box",
+        borderRadius: "20px",
+        "&::before": {
+          borderBottom: "0px"
+        },
+        "& > select:focus": {
+          backgroundColor: "#FFFFFF"
+        },
+        "&:hover": {
+          borderBottom: "0px"
+        },
+        marginRight: 8
+      }
+}
 
 const Card = () => {
     const preventDefault = (event) => event.preventDefault();
     const [isFavorite, setIsFavorite] = useState(false);
+    const useStyles = makeStyles(styles);
+    const classes = useStyles();
 
     let star = isFavorite ? 
     <StarIcon color="disabled" 
@@ -30,36 +55,51 @@ const Card = () => {
   return (
     <div className="safety-card">
       <Grid container spacing={3}>
-        <Grid item xs={0.5}>
-            {star}
+        <Grid item xs={3} >
+            <Grid container spacing ={3}>
+                <Grid item xs={1}>
+                    {star}
+                </Grid>
+
+                <Grid item xs={11}>
+                    <img style={{width: "100%"}} src={NightTime} alt="picture"/>
+                </Grid>
+
+            </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <img style={{width: "100%"}} src={NightTime} alt=""/>
-        </Grid>
-        <Grid item xs={4} style={{textAlign: "left"}}>
+        <Grid item xs={5} style={{textAlign: "left", paddingTop: "12px"}}>
             <div>
-                <FiberManualRecordIcon style={{color: "#F4BE5E", fontSize: 10}} /> 
-                <b><span style={{color: "#25345C"}}> Crash</span> <span style={{color: "#C4C4C4"}}>2.10G</span></b>
+                <FiberManualRecordIcon style={{color: "#F4BE5E", fontSize: 17}} /> 
+                <b><span style={{color: "#25345C", fontSize: "18px"}}> Crash</span> <span style={{color: "#C4C4C4", fontSize: "18px"}}>2.10G</span></b>
             </div>
-            <div className = "content" style={{color: "#C4C4C4", marginTop: "10px", marginBottom: "20px"}}>
+            <div className = "content">
                 <div>Jun 24, 2020 4:06 Am EOT</div>
                 <div>229  Alexandr Fedyachkin</div>
             </div>
 
-           
-            <Button variant="contained" size="large" className="liveButton safety-button">
+            <Button variant="contained" size="large" className="driverButton safety-button">
                 Drivers, Own
             </Button>
 
         </Grid>
-        <Grid item xs={3}>
+        <Grid item xs={4}>
             <Grid container>
-                <Grid item xs={12} style={{textAlign: "right"}}>
+                <Grid item xs={12} className="sideButton">
                     <Link href="#" onClick={preventDefault} className="dismissButton">
                         Dismiss
                     </Link>
                     <span style={{paddingLeft: "40px"}}></span>
-                    <Button className="reviewButton safety-button"><ExpandMoreIcon/> Review</Button>
+                    <FormControl variant="outlined">
+                        <Select style={{color: "#C4C4C4"}}
+                        native
+                        value={"1"}
+                        // onChange={handleChange}
+                        // label="Age"
+                        className={classes.selectForm}
+                        >
+                        <option value={1}>Review</option>
+                        </Select>
+                    </FormControl>
                 </Grid>
             </Grid>
         </Grid>
