0c23032c16339b54a4f4df8f9d09438b5e709f4d
Nam Nguyen Hoai <hoainamtin2@gmail.com>
2021-03-25 10:02:23 +0700

    pagingnation

--- src/components/Pagination/GenPaginationV1.jsx
+++ src/components/Pagination/GenPaginationV1.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { Row, Input, Pagination, PaginationItem, PaginationLink } from 'reactstrap';
-const DEFAULT_LIMIT = 50;
+import PaginationV2 from "components/Pagination/PaginationV2";
+import ArrowDownIcon from "components/Icons/ArrowDownIcon";
+import ArrowLeftIcon from "components/Icons/ArrowLeftIcon";
+import ArrowRightIcon from "components/Icons/ArrowRightIcon";
+import ArrowUpIcon from "components/Icons/ArrowUpIcon";
 
 export default class GenPaginationV1 extends Component {
 
@@ -9,7 +12,19 @@ export default class GenPaginationV1 extends Component {
 	render() {
 		return (
 			<>
-				<span>TODO</span>
+				<PaginationV2
+					pages={[
+						{ text: <ArrowDownIcon/>, arrow : true,disabled : true },
+						{ text: <ArrowLeftIcon/>, arrow : true,disabled : true },
+						{ active: true, text: 1 },
+						{ text: 2 },
+						{ text: 3 },
+						{ text: 4 },
+						{ text: 5 },
+						{ text: <ArrowRightIcon/>, arrow : true },
+						{ text: <ArrowUpIcon/>, arrow : true },
+					]}
+				/>
 			</>
 		);
 	}
