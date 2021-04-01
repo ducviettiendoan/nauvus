34ae090eeebb4fc15453c60715447d941c7ac6ed
Canh Minh <canhminh@Apples-MacBook-Air.local>
2021-04-01 16:59:57 +0700

    complete overview/drivers

--- src/components/CustomButtons/ToolboxButton.jsx
+++ src/components/CustomButtons/ToolboxButton.jsx
@@ -5,6 +5,7 @@ import TrashButton from "components/CustomButtons/TrashButton";
 import FilterButton from "components/CustomButtons/FilterButton";
 import EditButton from "components/CustomButtons/EditButton";
 import LinkButton from "components/CustomButtons/LinkButton";
+import ColumnButton from './ColumnButton';
 
 const styles = {
     headLeft: {
@@ -12,8 +13,8 @@ const styles = {
         alignItems: "center",
         justifyContent: "flex-end",
         "& > div": {
-          marginBottom: "0 !important",
-          marginRight: 8
+            marginBottom: "0 !important",
+            marginRight: 8
         }
     }
 };
@@ -25,11 +26,12 @@ export default function ToolboxButton(props) {
 
     return (
         <div className={classes.headLeft}>
-            <SettingSearchBox placeholder={ props.placeholder} />
-            { props.showFilter && <FilterButton /> }
-            { props.showEdit && <EditButton /> }
-            { props.showLink && <LinkButton /> }
-            { props.showTrash && <TrashButton /> }
+            <SettingSearchBox placeholder={props.placeholder} />
+            { props.showFilter && <FilterButton />}
+            { props.showEdit && <EditButton />}
+            { props.showLink && <LinkButton />}
+            { props.showTrash && <TrashButton />}
+            { props.showColumn && <ColumnButton />}
         </div>
     );
 };
--- src/reducers/overview.ts
+++ src/reducers/overview.ts
@@ -6,11 +6,13 @@ import { showLoading, hideLoading } from 'react-redux-loading-bar'
 
 // console.log(awsconfig);
 export const ACTION_TYPES = {
-  SET_OPEN_DRAWER: 'overview/SET_OPEN_DRAWER'
+  SET_OPEN_DRAWER: 'overview/SET_OPEN_DRAWER',
+  GET_DRIVERS_DATA: 'overview/GET_DRIVERS_DATA',
 };
 
 const initialState = {
-  openDrawer: false
+  openDrawer: false,
+  driversData: []
 };
 
 export type OverviewState = Readonly<typeof initialState>;
@@ -20,13 +22,45 @@ export default (state: OverviewState = initialState, action): OverviewState => {
   switch (action.type) {
     case ACTION_TYPES.SET_OPEN_DRAWER: {
       return {
+        ...state,
         openDrawer: action.payload
+      };
+    }
+    case ACTION_TYPES.GET_DRIVERS_DATA: {
+      return {
+        ...state,
+        driversData: action.payload
       };
     }
     default:
       return state;
   }
 };
+
+const dumpDriversData = () => {
+  let data = [];
+  for (let i = 0; i < 20; i++) {
+    let item = {
+      id: i + 2,
+      key: i + 2,
+      name: {
+        name: "Ali Singh",
+        id_1: "2447",
+        id_2: "4046921660",
+      },
+      drivingStatus: "Driving",
+      currentVehicle: "228",
+      currentLocation: {
+        distance: "8.1 mi SSE",
+        location: "Rockford, IL"
+      },
+      appVersion: "6959",
+      operatingSystem: "Android: 10"
+    };
+    data.push(item);
+  }
+  return data;
+}
 
 export const setOpenDrawer = (value) => async dispatch => {
   dispatch({
@@ -35,4 +69,10 @@ export const setOpenDrawer = (value) => async dispatch => {
   });
 };
 
+export const getDriversData = () => async dispatch => {
+  dispatch({
+    type: ACTION_TYPES.GET_DRIVERS_DATA,
+    payload: dumpDriversData
+  })
+}
 
