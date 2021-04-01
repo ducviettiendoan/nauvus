aa4393823bf099a6ad30a7ca0cd855c4fb4d78a3
Duy Nguyen Quang <duynguyen.benit@gmail.com>
2021-04-01 17:40:37 +0700

    create overview driver

--- src/reducers/overview.ts
+++ src/reducers/overview.ts
@@ -8,13 +8,15 @@ import { showLoading, hideLoading } from 'react-redux-loading-bar'
 export const ACTION_TYPES = {
   SET_OPEN_DRAWER: 'overview/SET_OPEN_DRAWER',
   GET_VEHICLE_DATA: 'overview/GET_VEHICLE_DATA',
-  GET_TRAILERS_DATA: 'overview/GET_TRAILERS_DATA'
+  GET_TRAILERS_DATA: 'overview/GET_TRAILERS_DATA',
+  GET_DRIVERS_DATA: 'overview/GET_DRIVERS_DATA',
 };
 
 const initialState = {
   openDrawer: false,
   vehiclesData: [],
-  trailersData: []
+  trailersData: [],
+  driversData: []
 };
 
 export type OverviewState = Readonly<typeof initialState>;
@@ -39,6 +41,12 @@ export default (state: OverviewState = initialState, action): OverviewState => {
         ...state,
         trailersData: action.payload
       }
+    }
+    case ACTION_TYPES.GET_DRIVERS_DATA: {
+      return {
+        ...state,
+        driversData: action.payload
+      };
     }
     default:
       return state;
@@ -96,6 +104,32 @@ const dumpTrailersData = () => {
   return data;
 }
 
+const dumpDriversData = () => {
+  let data = [];
+  for (let i = 0; i < 20; i++) {
+    let item = {
+      id: i + 2,
+      key: i + 2,
+      name: {
+        name: "Ali Singh",
+        id_1: "2447",
+        id_2: "4046921660",
+      },
+      drivingStatus: "Driving",
+      currentVehicle: "228",
+      currentLocation: {
+        distance: "8.1 mi SSE",
+        location: "Rockford, IL"
+      },
+      appVersion: "6959",
+      operatingSystem: "Android: 10"
+    };
+    data.push(item);
+  }
+  return data;
+}
+
+
 export const getVehiclesData = () => async dispatch => {
   dispatch({
     type: ACTION_TYPES.GET_VEHICLE_DATA,
@@ -109,5 +143,13 @@ export const getTrailersData = () => async dispatch => {
     payload: dumpTrailersData
   });
 }
+
+export const getDriversData = () => async dispatch => {
+  dispatch({
+    type: ACTION_TYPES.GET_DRIVERS_DATA,
+    payload: dumpDriversData
+  })
+}
+
 
 
--- src/views/pages/user/overview/Drivers.tsx
+++ src/views/pages/user/overview/Drivers.tsx
@@ -1,113 +1,142 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 // @material-ui/core components
 // import { makeStyles } from "@material-ui/core/styles";
-import { Theme, makeStyles } from '@material-ui/core';
-import { BaseCSSProperties } from '@material-ui/core/styles/withStyles';
+import {Theme, makeStyles, Card} from '@material-ui/core';
+import {BaseCSSProperties} from '@material-ui/core/styles/withStyles';
 
 // @material-ui/icons
 // import Weekend from "@material-ui/icons/Weekend";
-import FormatQuote from "@material-ui/icons/FormatQuote";
-import Search from "@material-ui/icons/Search";
-import InfoOutlined from "@material-ui/icons/InfoOutlined";
 // core components
-// import GridContainer from "components/Grid/GridContainer.js";
-// import GridItem from "components/Grid/GridItem.js";
-import Card from "components/Card/Card.js";
-import CardBody from "components/Card/CardBody.js";
-import CardFooter from "components/Card/CardFooter.js";
-import CardHeader from "components/Card/CardHeader.js";
 import Button from "components/CustomButtons/Button.js";
+import {connect} from 'react-redux';
+import {loadVehicles} from 'reducers/vehicle';
+import {IRootState} from 'reducers';
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import {getDriversData} from "reducers/overview"
+import DotIcon from "components/Icons/DotIcon";
+import ToolboxButton from "components/CustomButtons/ToolboxButton";
+import CloseIcon from "components/Icons/CloseIcon";
+import Chip from "@material-ui/core/Chip";
+import Grid from '@material-ui/core/Grid';
+import Table from "components/Table/TableV1";
 
-import KeyboardArrowDown from "@material-ui/icons/KeyboardArrowDown";
 
-import {
-  cardTitle,
-  roseColor
-} from "assets/jss/material-dashboard-pro-react.js";
-
-import CustomDropdown from "components/CustomDropdown/CustomDropdown.js";
-import Loading from "components/Loading/Loading";
-import { connect } from 'react-redux';
-import { loadVehicles } from 'reducers/vehicle';
-import { IRootState } from 'reducers';
-import imageTabs from "assets/img/Tabs.png";
-
-import VehicleAssets from "./components/VehicleAssets";
-
-import { Col, Row } from 'reactstrap';
 const styles = {
-  cardTitle,
-  cardTitleWhite: {
-    ...cardTitle,
-    color: "#FFFFFF",
-    marginTop: "0"
+  userRolesTitle: {
+    fontSize: 16,
+    color: "#25345C",
+    fontWeight: 700,
+    paddingRight: "8px !important"
   },
-  cardCategoryWhite: {
-    margin: "0",
-    color: "rgba(255, 255, 255, 0.8)",
-    fontSize: ".875rem"
+  selected: {
+    height: 24,
+    width: "auto",
+    background: "#ECEEF0 !important",
+    borderRadius: 28,
+    color: "#25345C !important",
+    display: "flex",
+    alignItems: "center",
   },
-  cardCategory: {
-    color: "#999999",
-    marginTop: "10px"
-  },
-  icon: {
-    color: "#333333",
-    margin: "10px auto 0",
-    width: "130px",
-    height: "130px",
-    border: "1px solid #E5E5E5",
-    borderRadius: "50%",
-    lineHeight: "174px",
-    "& svg": {
-      width: "55px",
-      height: "55px"
-    },
-    "& .fab,& .fas,& .far,& .fal,& .material-icons": {
-      width: "55px",
-      fontSize: "55px"
+  clearAll: {
+    textTransform: "none",
+    color: "#8097D8",
+    background: "unset !important",
+    boxShadow: "unset !important",
+    fontSize: 14,
+    fontWeight: 700,
+    padding: 0,
+    "&:hover": {
+      color: "#25345C"
     }
   },
-  iconRose: {
-    color: roseColor
+  chipSelected: {
+    display: "flex",
+    alignItems: "center",
+    paddingLeft: "0px !important"
   },
-  marginTop30: {
-    marginTop: "30px"
+  headContainer: {
+    alignItems: "center",
+    textAlign: "left",
+    marginTop: "8px"
   },
-  testimonialIcon: {
-    marginTop: "30px",
-    "& svg": {
-      width: "40px",
-      height: "40px"
+  headLeft: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    "& > div": {
+      marginBottom: "0 !important",
+      marginRight: 8
     }
   },
-  cardTestimonialDescription: {
-    fontStyle: "italic",
-    color: "#999999"
-  },
-  txtInfoMain: {
-    fontWeight: "bold",
-    fontSize: "18px",
-    lineHeight: "27px",
+  chips: {
+    background: "#ECEEF0",
     color: "#25345C",
-  },
-  txtInfoSub: {
-    fontSize: "14px",
-    lineHeight: "21px",
-    color: "#25345C",
-  },
-  txtNumberVehicle: {
-    fontSize: "14px",
-    lineHeight: "21px",
-    color: "#25345C",
-  },
-  txtSearchLabel: {
     fontSize: "12px",
-    lineHeight: "14px",
+    marginRight: 8,
+    fontWeight: 400,
+  },
+  tableRow: {
+    '&:nth-of-type(even)': {
+      backgroundColor: "#fbfbfb",
+    },
+  },
+  onHeaderRow: {
+    background: "#ECEEF0",
+  },
+  gridTitle: {
+    padding: "20px"
+  },
+  onHeaderCell: {
+    fontWeight: "bold"
+  },
+  alignItemsCenter: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "center"
+  },
+  drivingStatusContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  },
+  dotIcon: {
+    color: "#7CE7AC",
+    marginTop: 10
+  },
+  textTable: {
+    fontSize: '16px',
+    lineHeight: '21px',
     color: "#25345C",
-    textTransform: "uppercase",
-    fontWeight: "900"
-  }
+    fontFamily: "Lato",
+    fontStyle: "normal",
+    fontWeight: "normal",
+  },
+  textTableId: {
+    fontSize: '16px',
+    lineHeight: '21px',
+    color: "#C4C4C4",
+    fontFamily: "Lato",
+    fontStyle: "normal",
+    fontWeight: "normal",
+  },
+  textStatus: {
+    display: "inline-block",
+    fontSize: '14px',
+    lineHeight: '17px',
+    padding: "12px 16px",
+    color: "#27AE60",
+    background: "rgba(39, 174, 96, 0.1)",
+    borderRadius: 23,
+    fontWeight: "bold",
+  },
+  iconButton: {
+    '&:hover': {
+      color: '#25345C !important',
+    }
+  },
 };
 
 interface StyleProps {
@@ -120,82 +149,157 @@ const useStyles = makeStyles<Theme, StyleProps>(() => styles as any);
 export function Drivers(props) {
   const classes = useStyles({} as StyleProps);
 
-  React.useEffect(() => {
+  useEffect(() => {
+    props.getDriversData()
+  }, [])
+
+  useEffect(() => {
     async function fetchVehicles() {
       await props.loadVehicles();
     }
+
     fetchVehicles();
   }, [1]);
-  
+
+  const [chipData, setChipData] = useState([
+    {key: 0, label: 'Standard Admin'},
+    {key: 1, label: 'Full admin'},
+  ]);
+
+  const handleDelete = (chipToDelete) => () => {
+    setChipData((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
+  };
+
+  const handleClearAll = () => {
+    setChipData([])
+  }
+
+  const columns = [
+    {
+      title: 'Name',
+      key: 'name',
+      onHeaderCell: {className: classes.onHeaderCell},
+      render: user => (
+        <div className={classes.alignItemsCenter}>
+          <div className={classes.textTable}>{user.name}</div>
+          <div className={classes.textTableId}>{user.id_1}</div>
+          <div className={classes.textTableId}>{user.id_2}</div>
+        </div>
+      ),
+    },
+    {
+      title: 'Driving Status',
+      key: 'drivingStatus',
+      onHeaderCell: {className: classes.onHeaderCell},
+      render: drivingStatus => <div className={classes.drivingStatusContainer}>
+        <div><DotIcon className={classes.dotIcon}/></div>
+        <div className={classes.textTable}>{drivingStatus}</div>
+      </div>
+    },
+    {
+      title: 'Current Vehicle',
+      key: 'currentVehicle',
+      onHeaderCell: {className: classes.onHeaderCell},
+      render: currentVehicle => (
+        <div className={classes.alignItemsCenter}>
+          <div className={classes.textTable}>{currentVehicle}</div>
+        </div>
+      )
+    },
+    {
+      title: 'Current Location',
+      key: 'currentLocation',
+      onHeaderCell: {className: classes.onHeaderCell},
+      render: currentLocation => (
+        <div className={classes.alignItemsCenter}>
+          <div className={classes.textTable}>{currentLocation.distance}</div>
+          <div className={classes.textTable}>{currentLocation.location}</div>
+        </div>
+      )
+    },
+    {
+      title: 'App version',
+      key: 'appVersion',
+      onHeaderCell: {className: classes.onHeaderCell},
+      render: appVersion => (
+        <div className={classes.alignItemsCenter}>
+          <div className={classes.textTable}>{appVersion}</div>
+        </div>
+      )
+    },
+    {
+      title: 'Operating System',
+      key: 'operatingSystem',
+      onHeaderCell: {className: classes.onHeaderCell},
+      render: operatingSystem => (
+        <div className={classes.alignItemsCenter}>
+          <div className={classes.textTable}>{operatingSystem}</div>
+        </div>
+      )
+    },
+  ]
+
   return (
     <div>
-      <Row>
-          <Card>
-            <CardHeader>
-              <Row className="float-right mb-3">
-                <Button className="btn-more-actions">
-                  MORE ACTIONS <KeyboardArrowDown /> 
-                </Button>
-              </Row>
-              <Card>
-                <CardBody>
-                  <div className="ml-5">
-                    <div className={ classes.txtInfoMain}>We’ve moved your trailers</div>
-                    <div className={ `mb-4 ${classes.txtInfoSub}`}>
-                      241 trailers from your Trailers & Assets list in Settings have been moved to this list. 5 trailers witch matching names were automatically merged. The remaining 236 trailers were added.
-                    </div>
-                    <Button round className="btn-round-active w-150 mr-4">
-                      Keep all trailers
-                    </Button>
-                    <Button round className="btn-round-active w-150 mr-4">
-                      Delete
-                    </Button>
-                   </div>
-                   <div style={{ position: "absolute",top: "16px"}}>
-                     <InfoOutlined />
-                   </div>
-                </CardBody>
-              </Card>
+      <GridItem xs={12} sm={12} md={12}>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={12}>
+            {props.data.length > 0 && <Table
+              renderTitle={
+                <Grid container className={classes.gridTitle}>
+                  <Grid item xs={12} sm={12} md={6}>
+                    <Grid container className={classes.headContainer}>
+                      <Grid item xl={2} className={classes.userRolesTitle}> {chipData.length} selected for </Grid>
+                      <Grid item xl={10} className={classes.chipSelected}>
+                        {chipData.map(data => (
+                          <Chip
+                            deleteIcon={<CloseIcon/>}
+                            label={data.label}
+                            onDelete={handleDelete(data)}
+                            className={classes.chips}
+                          />
+                        ))}
+                        {chipData.length > 0 ?
+                          (
+                            <Button onClick={handleClearAll} className={classes.clearAll}>
+                              Clear All
+                            </Button>
+                          ) : ""}
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid xs={12} sm={12} md={6} className={classes.headLeft}>
+                    <ToolboxButton placeholder="Search for driver" showFilter showColumn/>
+                  </Grid>
+                </Grid>
+              }
+              columns={columns}
+              dataSource={props.data}
+              onHeaderRow={{
+                className: classes.onHeaderRow
+              }}
+              onBodyRow={{
+                className: classes.tableRow
+              }}
+            />
+            }
+          </GridItem>
+        </GridContainer>
+      </GridItem>
 
-              <Row className="mb-3">
-                <Col><img src={imageTabs} alt="..." /></Col>
-              </Row>
-
-              <Row>
-                <Col>
-                  <Button round className="btn-tags mr-2">
-                     Tags <KeyboardArrowDown /> 
-                   </Button>
-                   <Button round className="btn-tags">
-                     Gateway <KeyboardArrowDown /> 
-                   </Button>
-                </Col>
-                <Col>
-                  <div style={{ textAlign: 'right'}}>
-                    <span className={ classes.txtNumberVehicle }>{ props.vehicles.length } vehicles   </span> 
-                    <span className={ classes.txtSearchLabel }>{'  '}<Search /> search in vehicle</span>
-                  </div>
-                </Col>
-              </Row>
-            </CardHeader>
-            <CardBody>
-              <VehicleAssets data={ props.vehicles } /> 
-            </CardBody>
-            <CardFooter>
-            </CardFooter>
-          </Card>
-      </Row>
     </div>
   );
 }
 
 export default connect(
-  ({ authentication, vehicle }: IRootState) => ({
+  ({authentication, vehicle, overview}: IRootState) => ({
     isAuthenticated: authentication.isAuthenticated,
     user: authentication.user,
-    vehicles: vehicle.vehicles
+    vehicles: vehicle.vehicles,
+    data: overview.driversData
   }),
   {
-    loadVehicles
+    loadVehicles,
+    getDriversData
   }
 )(Drivers);
