2a3bc37ebe94911d91806eb01ee3fc92cbb048ea
Canh Minh <canhminh@Apples-MacBook-Air.local>
2021-04-01 16:07:12 +0700

    overview/assets

--- src/reducers/overview.ts
+++ src/reducers/overview.ts
@@ -6,11 +6,15 @@ import { showLoading, hideLoading } from 'react-redux-loading-bar'
 
 // console.log(awsconfig);
 export const ACTION_TYPES = {
-  SET_OPEN_DRAWER: 'overview/SET_OPEN_DRAWER'
+  SET_OPEN_DRAWER: 'overview/SET_OPEN_DRAWER',
+  GET_VEHICLE_DATA: 'overview/GET_VEHICLE_DATA',
+  GET_TRAILERS_DATA: 'overview/GET_TRAILERS_DATA'
 };
 
 const initialState = {
-  openDrawer: false
+  openDrawer: false,
+  vehiclesData: [],
+  trailersData: []
 };
 
 export type OverviewState = Readonly<typeof initialState>;
@@ -20,8 +24,21 @@ export default (state: OverviewState = initialState, action): OverviewState => {
   switch (action.type) {
     case ACTION_TYPES.SET_OPEN_DRAWER: {
       return {
+        ...state,
         openDrawer: action.payload
       };
+    }
+    case ACTION_TYPES.GET_VEHICLE_DATA: {
+      return {
+        ...state,
+        vehiclesData: action.payload
+      }
+    }
+    case ACTION_TYPES.GET_TRAILERS_DATA: {
+      return {
+        ...state,
+        trailersData: action.payload
+      }
     }
     default:
       return state;
@@ -34,5 +51,63 @@ export const setOpenDrawer = (value) => async dispatch => {
     payload: value
   });
 };
+
+const dumpVehiclesData = () => {
+  let data = [];
+  for (let i = 0; i < 25; i++) {
+    let item = {
+      id: i + 2,
+      key: i + 2,
+      name: `Vehicle 101 ${i + 1}`,
+      location: {
+        location: `Stoney Run Drive, 1.6 mi NW Severn, MD`,
+        time: "2 months ago"
+      },
+      lastTrip: "2 months ago",
+      status: 'Off',
+      fuel: "21%",
+      driver: "Ali Singh",
+      license: "2628PR",
+      tag: "Tegs"
+    };
+    data.push(item);
+  }
+  return data;
+}
+
+const dumpTrailersData = () => {
+  let data = [];
+  for (let i = 0; i < 15; i++) {
+    let item = {
+      id: i + 2,
+      key: i + 2,
+      name: `Vehicle 101 ${i + 1}`,
+      location: {
+        location: `Stoney Run Drive, 1.6 mi NW Severn, MD`,
+        time: "2 months ago"
+      },
+      lastTrip: "2 months ago",
+      status: 'Off',
+      battery: "60%",
+      tag: "Tags"
+    };
+    data.push(item);
+  }
+  return data;
+}
+
+export const getVehiclesData = () => async dispatch => {
+  dispatch({
+    type: ACTION_TYPES.GET_VEHICLE_DATA,
+    payload: dumpVehiclesData
+  });
+}
+
+export const getTrailersData = () => async dispatch => {
+  dispatch({
+    type: ACTION_TYPES.GET_TRAILERS_DATA,
+    payload: dumpTrailersData
+  });
+}
 
 
