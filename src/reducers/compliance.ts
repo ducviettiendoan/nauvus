53ab8d39e8b0a1135a7ad5d2a4aa70fe9b4a772b
Duy Nguyen Quang <duynguyen.benit@gmail.com>
2021-04-05 11:27:40 +0700

    complete compliance duty status summary

--- src/reducers/compliance.ts
+++ src/reducers/compliance.ts
@@ -2,10 +2,12 @@ export type ComplianceState = Readonly<typeof initialState>;
 
 export const ACTION_TYPES = {
   GET_HOS_AUDIT: 'compliance/GET_HOS_AUDIT',
+  GET_STATUS_SUMMARY: 'compliance/GET_STATUS_SUMMARY',
 }
 
 const initialState = {
   HOSAudit: [],
+  statusSummary: [],
 }
 
 export default (state: ComplianceState = initialState, action): ComplianceState => {
@@ -14,6 +16,12 @@ export default (state: ComplianceState = initialState, action): ComplianceState 
       return {
         ...state,
         HOSAudit: action.payload
+      };
+    }
+    case ACTION_TYPES.GET_STATUS_SUMMARY: {
+      return {
+        ...state,
+        statusSummary: action.payload
       };
     }
     default:
@@ -35,9 +43,36 @@ const HOSAuditData = () => {
   return data;
 }
 
+const statusSummaryData = () => {
+  let data = [];
+  for (let i = 0; i < 64; i++) {
+    let item = {
+      id: i + 1,
+      key: i + 1,
+      driver: "Ali Singh",
+      offDuty: "23:59",
+      sleeperBerth: "0:00",
+      driving: "0:00",
+      onDuty: "0:00",
+      yardMoveg: "0:00",
+      personalConveyanceg: "0:00"
+    };
+    data.push(item);
+  }
+  return data;
+}
+
 export const getHOSAudit = () => async dispatch => {
   dispatch({
     type: ACTION_TYPES.GET_HOS_AUDIT,
     payload: HOSAuditData
   });
 };
+
+export const getStatusSummary = () => async dispatch => {
+  dispatch({
+    type: ACTION_TYPES.GET_STATUS_SUMMARY,
+    payload: statusSummaryData
+  });
+};
+
