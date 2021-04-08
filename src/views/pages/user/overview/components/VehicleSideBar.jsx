c7c602ef661605959f2cfd8599880963dcce4304
Canh Minh <canhminh@Apples-MacBook-Air.local>
2021-04-08 17:13:08 +0700

    done features/overview-vehicle

--- src/views/pages/user/overview/components/VehicleSideBar.jsx
+++ src/views/pages/user/overview/components/VehicleSideBar.jsx
@@ -1,25 +1,21 @@
 import React from "react";
+import classNames from "classnames";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 // @material-ui/icons
 // import Weekend from "@material-ui/icons/Weekend";
-import FormatQuote from "@material-ui/icons/FormatQuote";
 // core components
-import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import Card from "components/Card/Card.js";
-import CardBody from "components/Card/CardBody.js";
-import CardFooter from "components/Card/CardFooter.js";
-import MailIcon from '@material-ui/icons/Mail';
 import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Accordion from "components/Accordion/Accordion";
+import ToolboxButton from "components/CustomButtons/ToolboxButton";
 
 import CameraIcon from "components/Icons/CameraIcon";
-import VehicleMapIcon from "components/Icons/VehicleMapIcon";
 import VehicleUserIcon from "components/Icons/VehicleUserIcon";
 import VehicleLinkIcon from "components/Icons/VehicleLinkIcon";
 import OpenInNewTabIcon from "components/Icons/OpenInNewTabIcon";
@@ -29,24 +25,60 @@ import PerfectScrollbar from "perfect-scrollbar";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 
 // import PerfectScrollbar from "react-perfect-scrollbar";
-import { Col, Row } from 'reactstrap';
 
 import { connect } from 'react-redux';
 import { loadVehicles, loadVehiclesMock } from 'reducers/vehicle';
+import { Grid } from "@material-ui/core";
+import Button from "components/CustomButtons/Button";
+import { MoreHoriz } from "@material-ui/icons";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Paper from "@material-ui/core/Paper";
+import Grow from "@material-ui/core/Grow";
+import Popper from "@material-ui/core/Popper";
+import customDropdownStyle from "assets/jss/material-dashboard-pro-react/components/adminNavbarLinksStyle.js";
+import CheckSquareOutlined from "components/Icons/CheckSquareOutlined";
+import Checkbox from "@material-ui/core/Checkbox";
+import { primaryColor } from "assets/jss/material-dashboard-pro-react";
+import Chip from "@material-ui/core/Chip";
+import CloseIcon from "components/Icons/CloseIcon";
 
-const styles = {
+const useStyles = makeStyles((theme) => ({
+  ...customDropdownStyle(theme),
+  sidebarContainer: {
+    overflowY: "auto"
+  },
+  dropdownContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "16px 16px 16px 16px !important",
+    color: "#25345C",
+    fontWeight: "bold"
+  },
+  moreAction: {
+    background: "#FFFFFF !important",
+    border: "1px solid #ECEEF0 !important"
+  },
+  sidebarHeader: {
+    width: "100%",
+    height: "65px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "16px 16px 16px 16px !important"
+  },
   cardContainer: {
-    width: "330px",
+    width: "auto",
     // height: "103px",
     border: "1px solid #ECEEF0",
     boxSizing: "border-box",
     borderRadius: "12px",
     background: "#FFFFFF",
-    margin: "auto",
-    marginBottom: "8px",
+    margin: "0px 15px 8px 15px !important"
   },
   liContainer: {
-    borderRadius: "12px",
     margin: "auto",
     display: "flex",
     alignItems: "center",
@@ -98,6 +130,14 @@ const styles = {
   expansionPanelClasses: {
     marginBottom: "4px !important"
   },
+  expansionPanelClassesRounded: {
+    borderBottomLeftRadius: "12px !important",
+    borderBottomRightRadius: "12px !important",
+    borderTopLeftRadius: "12px !important",
+    borderTopRightRadius: "12px !important",
+    border: "1px solid #ECEEF0",
+    boxShadow: "inherit"
+  },
   cardExpandContent: {
     padding: "16px 0px 16px 0px !important",
     background: "#ECEEF0",
@@ -112,10 +152,48 @@ const styles = {
   expandedHeader: {
     display: "flex",
     justifyContent: "space-between"
-  }
-};
+  },
+  dropdown: {
+    borderRadius: "12px",
+    boxShadow: "0px 1px 10px rgba(0, 0, 0, 0.25)",
+    width: "230px",
+    paddingLeft: "12px",
+    paddingRight: "12px",
+  },
+  popperInput: {
+    paddingBottom: "8px",
+    paddingTop: "8px",
+  },
+  dropdownItem: {
+    marginLeft: "8px",
+    fontWeight: 400,
+    fontSize: '12px',
+    color: '#25345C',
+  },
+  checked: {
+    color: primaryColor[0] + "!important"
+  },
+  checkRoot: {
+    padding: "10px",
+    "&:hover": {
+      backgroundColor: "unset"
+    }
+  },
+  itemContainer: {
+    display: "flex",
+    justifyContent: "flex-start",
+    paddingLeft: "0px !important"
+  },
+  chipContainer: {
+    paddingBottom: "16px !important"
+  },
+  chips: {
+    color: "#25345C",
+    fontSize: "12px",
+    marginRight: "5px"
+  },
+}))
 
-const useStyles = makeStyles(styles);
 var ps;
 export function VehicleSideBar(props) {
   const classes = useStyles();
@@ -151,13 +229,64 @@ export function VehicleSideBar(props) {
     props.loadVehiclesMock()
   }, [])
 
+  // popper
+  const [openMore, setOpenMore] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleCloseMore = () => setOpenMore(false)
+  const handleOpenMore = (event) => {
+    setOpenMore(true)
+    setAnchorEl(event.currentTarget);
+  }
+
+  const listTag = ["Tag 1", "Tag 2", "Tag 3"]
+
+  const [checked, setChecked] = React.useState([1]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+    setChipData([])
+    newChecked.filter((item) => item != 1).map((chip, i) => {
+      setChipData((prev) => [
+        ...prev,
+        {
+          key: i,
+          label: chip
+        }
+      ])
+    })
+  };
+
+  // chip
+  const [chipData, setChipData] = React.useState([]);
+
+  const handleDelete = (chipToDelete) => () => {
+    setChipData((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
+    const newChecked = checked.filter((chip) => chip !== chipToDelete.label)
+    console.log(chipToDelete)
+    setChecked(newChecked)
+  };
+
   return (
-    <div ref={mainPanelVehicleSideBar}>
+    <div ref={mainPanelVehicleSideBar} className={classes.sidebarContainer} >
       <Divider />
-      <Row>
-        <Col><div className={classes.txtMainDevice} style={{ padding: '23px' }}>Your recent for</div></Col>
-        <Col style={{ textAlign: 'right', padding: '23px', marginRight: '23px' }} className={classes.txtMainDevice}><div>Clear All</div></Col>
-      </Row>
+      <div>
+        <div>
+          <Grid xs={12} sm={12} md={12} className={classes.sidebarHeader}>
+            <ToolboxButton placeholder="Search asset" showFilter />
+          </Grid>
+          <Divider variant="fullWidth" light />
+        </div>
+      </div>
 
       <List>
         {/* {props.vehicles.map((vehicle, index) => (
@@ -179,6 +308,87 @@ export function VehicleSideBar(props) {
             } />
           </ListItem>
         ))} */}
+
+        <Grid className={classes.dropdownContainer}>
+          <Grid>Your recent for: </Grid> &nbsp;
+          <Grid>
+            <Button
+              color="white"
+              aria-label="edit"
+              justIcon
+              round
+              className={`btn-36 ${classes.moreAction} mr-2`}
+              onClick={handleOpenMore}
+            >
+              <MoreHoriz />
+            </Button>
+            <Popper
+              open={openMore}
+              anchorEl={anchorEl}
+              transition
+              disablePortal
+              placement="bottom-end"
+              className={classNames({
+                [classes.popperClose]: !anchorEl,
+                [classes.popperResponsive]: true,
+                [classes.popperNav]: true
+              })}
+            >
+              {({ TransitionProps }) => (
+                <Grow
+                  {...TransitionProps}
+                  id="profile-menu-list"
+                  style={{ transformOrigin: "0 0 0" }}
+                >
+                  <Paper className={classes.dropdown}>
+                    <ClickAwayListener onClickAway={handleCloseMore}>
+                      <MenuList role="menu">
+                        <Grid xs={12} sm={12} md={12} className={classes.popperInput}>
+                          <ToolboxButton placeholder="Search options" />
+                        </Grid>
+                        <Grid>Tags</Grid>
+
+                        {listTag.map((value) => {
+                          return (
+                            <MenuItem key={value} className={classes.itemContainer}>
+                              <div className={classes.dropdownItem}>
+                                <Checkbox
+                                  edge="end"
+                                  onChange={handleToggle(value)}
+                                  checked={checked.indexOf(value) !== -1}
+                                  checkedIcon={<CheckSquareOutlined />}
+                                  classes={{
+                                    checked: classes.checked,
+                                    root: classes.checkRoot
+                                  }}
+                                />
+                              </div>
+                              <div className={classes.dropdownItem}>
+                                {value}
+                              </div>
+                            </MenuItem>
+                          );
+                        })}
+                      </MenuList>
+                    </ClickAwayListener>
+                  </Paper>
+                </Grow>
+              )}
+            </Popper>
+          </Grid>
+        </Grid>
+
+        <GridItem className={classes.chipContainer} >
+          {chipData.map(data => (
+            <Chip
+              deleteIcon={<CloseIcon />}
+              label={data.label}
+              onDelete={handleDelete(data)}
+              className={classes.chips}
+            />
+          ))}
+        </GridItem>
+
 
         {props.vehicles.map((vehicle, index) => (
           <Card className={classes.cardContainer}>
@@ -232,7 +442,10 @@ export function VehicleSideBar(props) {
                 content: classes.expansionContentClasses
               }}
               expansionPanelClasses={{
-                root: classes.expansionPanelClasses
+                root: classes.expansionPanelClasses,
+              }}
+              expansionPanelRounded={{
+                rounded: classes.expansionPanelClassesRounded,
               }}
             />
           </Card>
