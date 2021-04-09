b86044e118706baf5087b9ee5c0e80bf63c48c74
Duc Doan <ducdoan@Apples-MacBook-Air.local>
2021-04-09 16:53:16 +0700

    update mock data fuel-energy

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -3,3 +3,6 @@ import "./setting-org";
 import "./vehicle";
 import "./setting-device";
 import "./setting-developer";
+import "./fuel-energy";
+import "./fuel-purchase";
+import "./driver-efficiency";
