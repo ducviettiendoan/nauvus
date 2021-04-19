7f522a15f3c62d3889ce81fc72eaf578ee0e8668
Duc Doan <ducdoan@Apples-MacBook-Air.local>
2021-04-19 14:09:00 +0700

    finish safety coaching and safety dashcam

--- src/reducers/safety.ts
+++ src/reducers/safety.ts
@@ -5,9 +5,13 @@ import { REQUEST, SUCCESS, FAILURE } from "../utils/action-type.util";
 export const ACTION_TYPES = {
   GET_USER_ROLES: "safety/GET_USER_ROLES",
   SET_SHOW_CRASH: "safety/SET_SHOW_CRASH",
+  GET_SAFETY_COACHING_DRIVER_QUEUE: "safety/GET_SAFETY_COACHING_DRIVER_QUEUE",
+  GET_SAFETY_DASH_CAM: "safety/GET_SAFETY_DASH_CAM",
 };
 
 const initialState = {
+  coachingDriverQueue: [],
+  dashCam: [],
   errorMessage: null,
   loading: false,
   showCrash: true,
@@ -19,11 +23,15 @@ export type SafetyState = Readonly<typeof initialState>;
 export default (state: SafetyState = initialState, action): SafetyState => {
   switch (action.type) {
     case REQUEST(ACTION_TYPES.GET_USER_ROLES):
+    case REQUEST(ACTION_TYPES.GET_SAFETY_COACHING_DRIVER_QUEUE):
+    case REQUEST(ACTION_TYPES.GET_SAFETY_DASH_CAM):
       return {
         ...state,
         loading: true,
       };
     case FAILURE(ACTION_TYPES.GET_USER_ROLES):
+    case FAILURE(ACTION_TYPES.GET_SAFETY_COACHING_DRIVER_QUEUE):
+    case FAILURE(ACTION_TYPES.GET_SAFETY_DASH_CAM):
       return {
         ...state,
         loading: false,
@@ -34,6 +42,21 @@ export default (state: SafetyState = initialState, action): SafetyState => {
         ...state,
         showCrash: action.payload,
       };
+
+    case SUCCESS(ACTION_TYPES.GET_SAFETY_COACHING_DRIVER_QUEUE): {
+      return {
+        ...state,
+        coachingDriverQueue: action.payload.data
+      };
+    }
+
+    case SUCCESS(ACTION_TYPES.GET_SAFETY_DASH_CAM): {
+      return {
+        ...state,
+        dashCam: action.payload.data
+      };
+    }
+
     default:
       return state;
   }
@@ -50,5 +73,19 @@ export const setShowCrash = (action) => async (dispatch) => {
   dispatch({
     type: ACTION_TYPES.SET_SHOW_CRASH,
     payload: action,
+  });
+};
+
+export const getCoachingDriverQueueData = (request) => async (dispatch) => {
+  dispatch({
+    type: ACTION_TYPES.GET_SAFETY_COACHING_DRIVER_QUEUE,
+    payload: axios.post("api/safety/coaching-driver-queue", request),
+  });
+};
+
+export const getDashCamData = (request) => async (dispatch) => {
+  dispatch({
+    type: ACTION_TYPES.GET_SAFETY_DASH_CAM,
+    payload: axios.post("api/safety/dash-cam", request),
   });
 };
