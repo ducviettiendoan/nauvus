86ae21eb9b0de36312c1c9c79ef43a2eb95f411f
Quang Minh <quangminh@Apples-MacBook-Air.local>
2021-05-20 14:45:14 +0700

    add alert contacts apis

--- src/reducers/setting-link-sharing.ts
+++ src/reducers/setting-link-sharing.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { REQUEST, SUCCESS, FAILURE } from "../utils/action-type.util";
+import dispatch from "./dispatch";
 
 export type SettingLinkSharingState = Readonly<typeof initialState>;
 
@@ -7,7 +8,9 @@ export type SettingLinkSharingState = Readonly<typeof initialState>;
 export const ACTION_TYPES = {
   //Alert Contact action type
   GET_ALERT_CONTACT: 'setting/link-sharing/GET_ALERT_CONTACT',
-
+  ADD_ALERT_CONTACT: 'setting/link-sharing/ADD_ALERT_CONTACT',
+  DELETE_ALERT_CONTACT: 'setting/link-sharing/DELETE_ALERT_CONTACT',
+  UPDATE_ALERT_CONTACT: 'setting/link-sharing/UPDATE_ALERT_CONTACT',
   //Schedule Report action type
   GET_SCHEDULE_REPORT: 'setting/link-sharing/GET_SCHEDULE_REPORT',
 
@@ -22,8 +25,13 @@ const initialState = {
   errorMessage: null,
   loading: false,
   //Alert Contact state
-  alertContacts: [],
-
+  alertContacts: {
+    data: [],
+    page: 0,
+    pageSize: 0,
+    total: 0
+  },
+  
   //Schedule Report state
   scheduleReports: [],
 
@@ -37,6 +45,9 @@ const initialState = {
 export default (state: SettingLinkSharingState = initialState, action): SettingLinkSharingState => {
   switch (action.type) {
     case REQUEST(ACTION_TYPES.GET_ALERT_CONTACT):
+    case REQUEST(ACTION_TYPES.ADD_ALERT_CONTACT):
+    case REQUEST(ACTION_TYPES.DELETE_ALERT_CONTACT):
+    case REQUEST(ACTION_TYPES.UPDATE_ALERT_CONTACT):
     case REQUEST(ACTION_TYPES.GET_SCHEDULE_REPORT):
     case REQUEST(ACTION_TYPES.GET_BY_ASSET):
     case REQUEST(ACTION_TYPES.GET_BY_LOCATION):
@@ -46,6 +57,9 @@ export default (state: SettingLinkSharingState = initialState, action): SettingL
         loading: true,
       };
     case FAILURE(ACTION_TYPES.GET_ALERT_CONTACT):
+    case FAILURE(ACTION_TYPES.ADD_ALERT_CONTACT):
+    case FAILURE(ACTION_TYPES.DELETE_ALERT_CONTACT):
+    case FAILURE(ACTION_TYPES.UPDATE_ALERT_CONTACT):
     case FAILURE(ACTION_TYPES.GET_SCHEDULE_REPORT):
     case FAILURE(ACTION_TYPES.GET_BY_ASSET):
     case FAILURE(ACTION_TYPES.GET_BY_LOCATION):
@@ -59,9 +73,19 @@ export default (state: SettingLinkSharingState = initialState, action): SettingL
     case SUCCESS(ACTION_TYPES.GET_ALERT_CONTACT): {
       return {
         ...state,
-        alertContacts: action.payload.data
+        alertContacts: {
+          data: action.payload.data.data,
+          page: action.payload.data.page.page,
+          pageSize: action.payload.data.page.per_page,
+          total: action.payload.data.data.length
+        }
       };
     }
+
+    case SUCCESS(ACTION_TYPES.ADD_ALERT_CONTACT): {
+      console.log(action.payload)
+    }
+
 
     //Schedule Report Reducer
     case SUCCESS(ACTION_TYPES.GET_SCHEDULE_REPORT): {
@@ -100,9 +124,30 @@ export default (state: SettingLinkSharingState = initialState, action): SettingL
 export const getAlertContact = (request) => async dispatch => {
   dispatch({
     type: ACTION_TYPES.GET_ALERT_CONTACT,
-    payload: axios.get("/api/alert-contacts"),
+    payload: axios.get(`/api/alert-contacts`, request),
   });
 };
+
+export const addAlertContact = (request) => async dispatch => {
+  dispatch({
+    type: ACTION_TYPES.ADD_ALERT_CONTACT,
+    payload: axios.post(`/api/alert-contacts`, request)
+  })
+}
+
+export const deleteAlertContact = (params) => async dispatch => {
+  dispatch({
+    type: ACTION_TYPES.ADD_ALERT_CONTACT,
+    payload: axios.delete(`/api/alert-contacts?id=${params}`)
+  })
+}
+
+export const updateAlertContact = (request) => async dispatch => {
+  dispatch({
+    type: ACTION_TYPES.ADD_ALERT_CONTACT,
+    payload: axios.put(`/api/alert-contacts`, request)
+  })
+}
 
 //Schedule Report Action
 export const getScheduleReport = (request) => async dispatch => {
